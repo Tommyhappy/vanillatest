970b9d97f301f8c9474d2013216a6012ca45ca38
Tommyhappy <33391400+Tommyhappy@users.noreply.github.com>
2017-11-13 12:02:25 +0800

    Create input.java

---
+++ input.java
@@ -0,0 +1,16 @@
+package jp.linebot;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Input {
+
+    private Events[] events;
+
+}
